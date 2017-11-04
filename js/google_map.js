71d012e27be3b1f0f658e386e68e4f8c42bb8ddd
Kyle Rubenok <kyle@rubenok.ca>
2017-11-04 16:54:13 -0700

    Back to normal
    This reverts commit 94db0975cabdfe804d9fe97c78ad601110edc680.

--- js/google_map.js
+++ js/google_map.js
@@ -39,7 +39,7 @@ function init() {
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/M pin (1).png'
+                icon: 'images/loc.png'
             });
 
         });
